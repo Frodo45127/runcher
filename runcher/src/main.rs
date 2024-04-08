ba088aed6aec8e407a48452c545765055853f327
Frodo45127 <frodo_gv@hotmail.com>
2024-04-08 03:07:11 +0200

    Updated license year.

--- runcher/src/main.rs
+++ runcher/src/main.rs
@@ -1,8 +1,8 @@
 //---------------------------------------------------------------------------//
-// Copyright (c) 2017-2023 Ismael Gutiérrez González. All rights reserved.
+// Copyright (c) 2017-2024 Ismael Gutiérrez González. All rights reserved.
 //
 // This file is part of the Rusted Launcher (Runcher) project,
-// which can be found here&: https://github.com/Frodo45127/runcher.
+// which can be found here: https://github.com/Frodo45127/runcher.
 //
 // This file is licensed under the MIT license, which can be found here:
 // https://github.com/Frodo45127/runcher/blob/master/LICENSE.
--- workshopper/src/commands/mod.rs
+++ workshopper/src/commands/mod.rs
@@ -1,7 +1,7 @@
 //---------------------------------------------------------------------------//
-// Copyright (c) 2017-2023 Ismael Gutiérrez González. All rights reserved.
+// Copyright (c) 2017-2024 Ismael Gutiérrez González. All rights reserved.
 //
-// This file is part of the Rusted PackFile Manager (RPFM) project,
+// This file is part of the Rusted Launcher (Runcher) project,
 // which can be found here: https://github.com/Frodo45127/rpfm.
 //
 // This file is licensed under the MIT license, which can be found here:
